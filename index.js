c13a7e24a50b4fe4acd96b1f9813e8331f91d9a2
pmpatelmech <68679992+pmpatelmech@users.noreply.github.com>
2022-10-06 22:56:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const app = express();
+const ab = require("./routes")
+app.use('/api',)
---
+++ routes.js
@@ -0,0 +1,7 @@
+const { Router } = require("express")
+
+route.get('/example1', (req, res) => {
+    res.send("route get working")
+})
+
+module.exports = routes
